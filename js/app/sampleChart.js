b90d027119c735abc00a4163fdac205ff14dd5d3
lichtteil <info@lichtteilchen.de>
2017-05-20 18:49:48 +0200

    Use combined chart as example

--- js/app/sampleChart.js
+++ js/app/sampleChart.js
@@ -3,22 +3,27 @@
 define(['jquery', 'https://www.gstatic.com/charts/loader.js'], function($) {
 	return function () {
 
-		google.charts.load("43", {packages:["bar"]});
+		google.charts.load('current', {'packages':['corechart']});
 		google.charts.setOnLoadCallback(drawChart);
 
 		function drawChart() {
 			var data = google.visualization.arrayToDataTable([
-			['', 'Mein Unternehmen', 'Branchenvergleich'],
-			['Drucker',  1000,      400],
-			['Lampen',  1170,      460],
-			['Server',  660,       1120],
-			['Klimatechnik',  1030,      540]
-		]);
+				['Typ', 'Eigener Verbrauch', 'Mitbewerber mit geringstem Verbrauch', 'Branchen-Durchschnitt'],
+				['Leuchtmittel',	900,	938,	950],
+				['Drucker',			1300,	1120,	1200],
+				['Monitore',		1167,	1207,	1180],
+				['Server',			1210,	1010,	1100],
+				['2008/09',			991,	1191,	1000]
+			  ]);
 
-		var options = {
-		};
+			var options = {
+				vAxis: {title: 'Kosten/€'},
+				hAxis: {title: 'Verbrauchertyp'},
+				seriesType: 'bars',
+				series: {2: {type: 'steppedArea'}}
+			};
 
-		var chart = new google.charts.Bar(document.getElementById('chart_div'));
+			var chart = new google.visualization.ComboChart(document.getElementById('chart_div'));
 			chart.draw(data, options);
 		}
 
