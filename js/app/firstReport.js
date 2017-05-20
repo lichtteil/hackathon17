b6bc127d660b5918c9de8c8d10ee6e0e8074cf75
lichtteil <info@lichtteilchen.de>
2017-05-20 19:24:03 +0200

    Add JS for first report

---
+++ js/app/firstReport.js
@@ -0,0 +1,31 @@
+
+
+define(['jquery', 'https://www.gstatic.com/charts/loader.js'], function($) {
+	return function () {
+
+		google.charts.load('current', {'packages':['corechart']});
+		google.charts.setOnLoadCallback(drawChart);
+
+		function drawChart() {
+			var data = google.visualization.arrayToDataTable([
+				['Typ', 'Eigener Verbrauch', 'Mitbewerber mit geringstem Verbrauch', 'Branchen-Durchschnitt'],
+				['Leuchtmittel',	900,	938,	950],
+				['Drucker',			1300,	1120,	1200],
+				['Monitore',		1167,	1207,	1180],
+				['Server',			1210,	1010,	1100],
+				['2008/09',			991,	1191,	1000]
+			  ]);
+
+			var options = {
+				vAxis: {title: 'Kosten/€'},
+				hAxis: {title: 'Verbrauchertyp'},
+				seriesType: 'bars',
+				series: {2: {type: 'steppedArea'}}
+			};
+
+			var chart = new google.visualization.ComboChart(document.getElementById('chartFirstReport'));
+			chart.draw(data, options);
+		}
+
+	};
+});
