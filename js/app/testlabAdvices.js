455e0a689304370c1ccee93e775ec08c09c53c08
lichtteil <info@lichtteilchen.de>
2017-05-20 22:32:47 +0200

    Define draggable and droppable for advices

--- js/app/testlabAdvices.js
+++ js/app/testlabAdvices.js
@@ -3,8 +3,13 @@
 define(['jquery', 'https://code.jquery.com/ui/1.12.1/jquery-ui.min.js'], function($) {
 	return function () {
 
-		$('.advice').draggable({
+		$('.adviceDroppable, #lightsSlider').droppable({
+			accept: ".advice"
+		});
 
+
+		$('.advice').draggable({
+			revert: 'invalid'
 		});
 
 	};
