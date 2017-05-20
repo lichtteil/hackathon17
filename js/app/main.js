454d38da1f10d2968d7a7808a2c5305d5e9b85e5
lichtteil <info@lichtteilchen.de>
2017-05-20 14:17:40 +0200

    Add sample chart

--- js/app/main.js
+++ js/app/main.js
@@ -1,7 +1,29 @@
 
 
-define(['jquery'], function($) {
-    $(function() {
-        console.log('hello');
-    });
+define(['jquery', 'https://www.gstatic.com/charts/loader.js'], function($) {
+	$(function() {
+		console.log('hello');
+	});
+
+	google.charts.load("43", {packages:["bar"]});
+	google.charts.setOnLoadCallback(drawChart);
+
+	function drawChart() {
+		var data = google.visualization.arrayToDataTable([
+		['Year', 'Sales', 'Expenses'],
+		['2013',  1000,      400],
+		['2014',  1170,      460],
+		['2015',  660,       1120],
+		['2016',  1030,      540]
+	]);
+
+	var options = {
+		title: 'Company Performance',
+		hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+		vAxis: {minValue: 0}
+	};
+
+	var chart = new google.charts.Bar(document.getElementById('chart_div'));
+		chart.draw(data, options);
+	}
 });
